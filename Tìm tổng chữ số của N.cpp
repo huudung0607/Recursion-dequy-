33f8dde189409bff9d75194282aae5fac3c52b1b
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2024-12-23 20:24:44 +0700

    Create Tìm tổng chữ số của N.cpp

---
+++ Tìm tổng chữ số của N.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int S(long long n)
+{
+	if (n < 10)
+	{
+		return n;
+	}
+	return n % 10 + S(n / 10);
+}
+int main()
+{
+	long long n;
+	cin >> n;
+	cout << S(n) << endl;
+}
