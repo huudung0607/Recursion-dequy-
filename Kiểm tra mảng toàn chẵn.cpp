c26c82926c93c5714519c0f7c388f55e5417b6cb
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2024-12-27 20:33:55 +0700

    Create Kiểm tra mảng toàn chẵn.cpp

---
+++ Kiểm tra mảng toàn chẵn.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <string>
+using namespace std;
+const int mod = 1e9 + 7;
+
+bool check(int a[], int i, int n)
+{
+	if (i == n)
+	{
+		return true;
+	}
+	else if (a[i] % 2 == 1)
+	{
+		return false;
+	}
+	else
+	{
+		return check(a, i + 1, n);
+	}
+}
+int a[1000001];
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	if (check(a,0,n))
+	{
+		cout << "YES";
+	}
+	else
+	{
+		cout << "NO";
+	}
+}
